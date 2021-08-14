6bbc28fd4757b204a41d5fd05bb90a9d9fd918a9
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2021-08-14 15:46:54 +0200

    Début du passage sous react
    Ajout de la nav bar, premiers tests de pagination.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Route} from 'react-router-dom';
+import React from "react";
+import Home from './Home';
+import Contacts from './Contacts';
+import NavBar from './NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+     <Route exact path="/" component={Home}/>
+     <Route exact path="/contacts" component={Contacts}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Contacts.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+<p>Contacts</p>
+  );
+}
+
+export default App;
