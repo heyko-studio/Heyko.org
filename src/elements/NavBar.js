9e43c9312de578097946c4ac300c3520f621335f
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2021-09-07 18:25:27 +0200

    test

--- src/elements/NavBar.js
+++ src/elements/NavBar.js
@@ -1,5 +1,39 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+var image_link = 'https://images-eu.ssl-images-amazon.com/images/I/81BES%2BtsVvL.png';
+
+GetUserProfileImage();
+/*
+function GetUserProfileImage()
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://heyko.alwaysdata.net/requests/get_user_avatar", false ); // false for synchronous request
+    xmlHttp.send("test");
+    return xmlHttp.responseText;
+}
+*/
+function GetUserProfileImage() {
+  let x = new XMLHttpRequest();
+x.open("POST", `https://heyko.alwaysdata.net/requests/get_user_avatar`);
+x.setRequestHeader("Content-type", "application/json");
+let params = {
+}
+x.send(JSON.stringify(params))
+
+x.onreadystatechange = (e) => {
+  console.log(x.responseText)
+}
+  }
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <img className="profile_img" id="profile_img" src={image_link} alt="Heyko profile"></img>
+      </div>
+    );
+  }
+}
 
 function NavBar() {
   var number = Math.floor(Math.random() * 100)
@@ -9,7 +43,7 @@ function NavBar() {
     NavBar()
   }
     return(
-      
+      <div>
         <header className="topbar ombre">
       
   <div className="topbar-container">
@@ -29,12 +63,16 @@ function NavBar() {
         <nav className="menu">
         <div className="topbar-container">
           <div id="profile_image">
-            <img src="./img/profile.png" alt="Heyko profile" width="50px"></img>
+       <HelloMessage/>
+        {
+        document.getElementById('hello-example')
+         }
         </div>
         </div>
         </nav>
         </div>
         </header>
+        </div>
     )
 }
 /*
