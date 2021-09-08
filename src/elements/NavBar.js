2a0596313051c84826832d262a6c1d93e6fe336f
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2021-09-08 17:41:41 +0200

    te

--- src/elements/NavBar.js
+++ src/elements/NavBar.js
@@ -1,39 +1,5 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-var image_link = 'https://images-eu.ssl-images-amazon.com/images/I/81BES%2BtsVvL.png';
-
-GetUserProfileImage();
-/*
-function GetUserProfileImage()
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://heyko.alwaysdata.net/requests/get_user_avatar", false ); // false for synchronous request
-    xmlHttp.send("test");
-    return xmlHttp.responseText;
-}
-*/
-function GetUserProfileImage() {
-  let x = new XMLHttpRequest();
-x.open("POST", `https://heyko.alwaysdata.net/requests/get_user_avatar`);
-x.setRequestHeader("Content-type", "application/json");
-let params = {
-}
-x.send(JSON.stringify(params))
-
-x.onreadystatechange = (e) => {
-  console.log(x.responseText)
-}
-  }
-
-class HelloMessage extends React.Component {
-  render() {
-    return (
-      <div>
-        <img className="profile_img" id="profile_img" src={image_link} alt="Heyko profile"></img>
-      </div>
-    );
-  }
-}
 
 
 
@@ -126,10 +92,7 @@ function NavBar() {
         <nav className="menu">
         <div className="topbar-container">
           <div id="profile_image">
-
        <Profile/>
-
-
         {
         document.getElementById('hello-example')
          }
