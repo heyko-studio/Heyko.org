18ecd75a6323863283b71485d7399e2b416ec5cc
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2021-09-26 17:05:00 +0200

    adding a Captcha to the contact form

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,25 +1,36 @@
+import ReCAPTCHA from "react-google-recaptcha";
+import React from 'react';
+const recaptchaRef = React.createRef();
+
 function App() {
+  
     function handleSubmit(e) {
         e.preventDefault();
-        console.log("t")
         const first_name = document.getElementById("first_name").value
         const last_name = document.getElementById("last_name").value
         const email = document.getElementById("email").value
         const subject = document.getElementById("subject").value
         const content = document.getElementById("content").value
+        const recaptcha = recaptchaRef.current.getValue()
 
+        if (recaptcha) {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: `{"first_name":"${first_name}","last_name":"${last_name}","email":"${email}","subject":"${subject}","content":"${content}"}`
+            body: `{"first_name":"${first_name}","last_name":"${last_name}","email":"${email}","subject":"${subject}","content":"${content}", "captcha":"${recaptcha}"}`
         };
        fetch(`https://backend.heyko.fr/requests/send_mail?`, requestOptions)
             .then(response => response.json())
             .then(data => console.log(data))  
       }
+      else {
+        console.log(recaptcha)
+      }
+    }
   return (
+      <>
     <div>
-        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 <img width="700px" className="heyko_banner_1" src="./img/heyko.png" alt="Heyko banner"></img>
 
 
@@ -38,14 +49,18 @@ function App() {
             <div className="form-line3">
                 <textarea id="content" placeholder="Your message" required name="message"></textarea>
             </div>
-            <div class="g-recaptcha" data-sitekey="6LdOR5AcAAAAAF80awkaFKClF2lkD95wsmvYyBWX"></div>
+            <ReCAPTCHA sitekey="6LdOR5AcAAAAAF80awkaFKClF2lkD95wsmvYyBWX" ref={recaptchaRef}/>
         <div className="send-container">
             <button className="send" name="submit" id="text2">Send</button>
         </div>
         </form>
         </section>
 </div>
+
+
+</>
   );
+  
 
 }
 export default App;
