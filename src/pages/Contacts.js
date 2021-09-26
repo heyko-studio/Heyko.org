2bfd9c40377f12e0feb93d728518aeec1a20245f
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2021-09-26 13:52:38 +0200

    contact form added

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,26 +1,44 @@
 function App() {
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("t")
+        const first_name = document.getElementById("first_name").value
+        const last_name = document.getElementById("last_name").value
+        const email = document.getElementById("email").value
+        const subject = document.getElementById("subject").value
+        const content = document.getElementById("content").value
 
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: `{"first_name":"${first_name}","last_name":"${last_name}","email":"${email}","subject":"${subject}","content":"${content}"}`
+        };
+       fetch(`https://backend.heyko.fr/requests/send_mail?`, requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data))  
+      }
   return (
     <div>
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <img width="700px" className="heyko_banner_1" src="./img/heyko.png" alt="Heyko banner"></img>
 
 
 <section className="form-container">
     <h2 id="text1">Contact our team</h2>
     <div className="hr"></div>
-    <form className="contact-form">
-    <div class="form-line1">
-                <input className="input_1" id="text8" type="text" placeholder="First name" required autocomplete="name" name="first name"></input>
-                <input className="input_1" id="text9" type="text" placeholder="Last name" required autocomplete="family-name" name="last name"></input>
+    <form className="contact-form" onSubmit={handleSubmit}>
+    <div className="form-line1">
+                <input className="input_1" id="first_name" type="text" placeholder="First name" required autoComplete="name" name="first name"></input>
+                <input className="input_1" id="last_name" type="text" placeholder="Last name" required autoComplete="family-name" name="last name"></input>
             </div>
-            <div class="form-line2">
-                <input className="input_1" type="email" placeholder="Email address" autocomplete="email" required name="email"></input>
-                <input className="input_1" id="text10" type="text" placeholder="Subject" required name="sujet"></input>
+            <div className="form-line2">
+                <input className="input_1" id="email" type="email" placeholder="Email address" autoComplete="email" required name="email"></input>
+                <input className="input_1" id="subject" type="text" placeholder="Subject" required name="sujet"></input>
             </div>
-            <div class="form-line3">
-                <textarea id="text11" placeholder="Your message" required name="message"></textarea>
+            <div className="form-line3">
+                <textarea id="content" placeholder="Your message" required name="message"></textarea>
             </div>
-            <div class="g-recaptcha" data-sitekey="6Lc_66QZAAAAAELv4lDy56GVpQ8HSVIxkZAsdsBK"></div>
+            <div class="g-recaptcha" data-sitekey="6LdOR5AcAAAAAF80awkaFKClF2lkD95wsmvYyBWX"></div>
         <div className="send-container">
             <button className="send" name="submit" id="text2">Send</button>
         </div>
