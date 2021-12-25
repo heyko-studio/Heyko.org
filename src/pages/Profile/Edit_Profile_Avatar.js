de696aee6fc091cdf61f80c81c247949b5bfb283
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2021-12-25 15:48:48 +0100

    update

--- src/pages/Profile/Edit_Profile_Avatar.js
+++ src/pages/Profile/Edit_Profile_Avatar.js
@@ -1,8 +1,36 @@
 import React from 'react'
 import './Profile.css';
 import Canvas from '../welcome/Canvas'
+import ReactDOM from 'react-dom';
 
 function App() {
+  var error_screen = 0
+  setTimeout(function() {
+    reportWindowSize()
+    }, 100)
+    function reportWindowSize() {
+        if (window.innerHeight < 516) {
+            if (error_screen !== 1) {
+                error_screen = 1
+                ReactDOM.render(
+                    <>
+                    <div className="white_background">
+                    </div>
+                    <div className="default_message wrong">Please rotate your phone</div>
+                    </>,
+                    document.getElementById('error_screen')
+                );
+            }
+        }
+        else {
+            if (error_screen === 1) {
+                error_screen = 0
+                ReactDOM.unmountComponentAtNode(document.getElementById("error_screen")) 
+            }
+        }
+    }
+
+    window.onresize = reportWindowSize;
     return(
         <>
                 <div className="Welcome transition_3" id="Welcome_Page">
