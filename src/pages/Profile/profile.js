6f56630c0573decf36a9a590355decc49817fe79
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2022-01-11 12:44:21 +0100

    update

--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -53,9 +53,9 @@ fetch(`https://backend.heyko.fr/requests/get_likes`, requestOptions)
     <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd" />
   </svg>
-  <svg className="Profile number_icons Button" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-</svg>
+        <div id="Follow Button Contener">
+
+        </div>
         </>
       );
     }
@@ -530,6 +530,12 @@ function connect_result(data, data2) {
             }
           }
           else {
+            ReactDOM.render(
+              <svg className="Profile number_icons Button" style={{marginLeft: "10px"}} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+            </svg>,
+              document.getElementById('Follow Button Contener')
+            );
             if (data.discord_account === true) {
               ReactDOM.render(
                 <img alt="discord logo" className='Profile Discord Button' src="https://heyko.fr/img/discord.svg"></img>,
