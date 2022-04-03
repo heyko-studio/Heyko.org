83898c6b656faffef8df20f95307dacb059cf82c
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-04-03 17:59:27 +0200

    ✨ Updating the likes counter when the user likes

--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -42,7 +42,7 @@ fetch(`https://backend.heyko.fr/requests/get_likes`, requestOptions)
     render() {
       return (
         <>
-          <p id="user_likes" className="Profile icon_text">{this.state.user_likes}</p>
+          <p id="user_likes" className="Profile icon_text">{this.state.user_likes || 0}</p>
   <svg id="User Like Button" className="Profile number_icons button" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
   </svg>
@@ -58,157 +58,6 @@ fetch(`https://backend.heyko.fr/requests/get_likes`, requestOptions)
       );
     }
   }
-  /*class User_Shop extends React.Component {
-    render() {
-      return (
-        <div className="Profile User_shop">
-  <svg className="Profile Shop_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-  </svg>
-  <p className="Profile Shop_icon_text">User Shop</p>
-  <hr className="hr_3"></hr>
-  <p className="Profile icon_text_numbers">25</p>
-  <svg className="Profile Shop_icon_numbers" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-  </svg>
-  <p className="Profile icon_text_numbers">42</p>
-  <svg className="Profile Shop_icon_numbers" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
-  </svg>
-        <a href="store" className="button view Profile decal_1">View</a>
-        </div>
-      )
-    }
-  }
-  */
- /*
-  class Activity extends React.Component {
-    render() {
-      return (
-        <div className="Profile Activity">
-      <svg className="Profile Success_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-  </svg>
-        <p className="Profile Rewards">Activity (Last 7 days)</p>
-        <hr className="hr_3"></hr>
-        <svg className="Activity messages_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-  </svg>
-  <p className="Activity numbers">2563</p>
-  
-  
-  <svg className="Activity messages_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-  <p className="Activity numbers">16 hours</p>
-  
-  <svg className="Activity messages_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-  </svg>
-  <p className="Activity numbers">24</p>
-  
-  
-  <svg className="Activity messages_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
-  </svg>
-  <p className="Activity numbers">3</p>
-  
-  
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <div id="chart_div"></div>
-    <div style={{ display: 'flex', maxWidth: 900 }}>
-    <Chart
-    className="Activity Chart"
-    width={'600px'}
-    height={'90px'}
-    chartType="LineChart"
-    loader={<div>Loading Chart</div>}
-    data={[
-      ['x', ''],
-      [0, 0],
-      [1, 10],
-      [2, 23],
-      [3, 17],
-      [4, 18],
-      [5, 9],
-      [6, 11],
-      [7, 27],
-      [8, 33],
-      [9, 40],
-      [10, 32],
-      [11, 35],
-      [12, 10],
-      [13, 23],
-      [14, 17],
-      [15, 18],
-      [16, 9],
-      [17, 11],
-      [18, 27],
-      [19, 33],
-      [20, 40],
-      [21, 32],
-      [22, 35]
-    ]}
-    options={{
-      colors: ['rgb(42, 160, 91)'],
-      legend: 'none',
-    }}
-    rootProps={{ 'data-testid': '1' }}
-  />
-  </div>
-        </div>
-      )
-    }
-  }
-  */
- /*
-  class Success extends React.Component {
-    render() {
-      return (
-        <>
-            <div className="Profile Success">
-            <svg className="Profile Success_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
-  </svg>
-        <p className="Profile Rewards">Rewards</p>
-        <hr className="hr_3"></hr>
-        <div className="Rewards_box">
-        <svg className="Profile Arrows Left_Arrow" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-  </svg>
-  <svg className="Profile Arrows Right_Arrow" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-  </svg>
-  <svg className="Profile Rewards_icon gold" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
-  </svg>
-  <svg className="Profile Rewards_icon bronze" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-  </svg>
-  <svg className="Profile Rewards_icon gold" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-  </svg>
-  <svg className="Profile Rewards_icon silver" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-  </svg>
-  <svg className="Profile Rewards_icon bronze" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-  </svg>
-  <svg className="Profile Rewards_icon bronze" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-  </svg>
-  <svg className="Profile Rewards_icon silver" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
-  </svg>
-  </div>  
-      </div>
-        </>
-      );
-    }
-  }
-  */
   const history = useHistory();
   const profile_id = window.location.href.split("/")[window.location.href.split("/").length - 1]
   if (profile_id.split("connect").length > 1) {
@@ -497,6 +346,7 @@ ReactDOM.render(
           function like() {
             ReactDOM.unmountComponentAtNode(document.getElementById("button_like_contener"))
             document.getElementById("User Like Button").style.color = "#e66a89"
+            document.getElementById("user_likes").innerHTML = parseInt(document.getElementById("user_likes").innerHTML) + 1
             const requestOptions2 = {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
